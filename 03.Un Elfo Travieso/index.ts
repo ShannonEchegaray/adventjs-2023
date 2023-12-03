93678d6e828520909ba698b7fdfc57744148d972
Shannon Echegaray <shannonechegaray@gmail.com>
2023-12-03 14:20:24 -0300

    feat: add 3rd day

---
+++ 03.Un Elfo Travieso/index.ts
@@ -0,0 +1,17 @@
+function findNaughtyStep(original: string, modified: string): string {
+
+  const max = original.length >= modified.length ? original : modified;
+
+  for(let i = 0; i < max.length; i++){
+    const originalChar = original[i];
+    const modifiedChar = modified[i];
+
+    if(originalChar === modifiedChar){
+      continue;
+    }
+    return original.length > modified.length
+      ? originalChar
+      : modifiedChar
+  }
+  return ''
+}
